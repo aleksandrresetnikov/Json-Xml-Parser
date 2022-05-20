39448a1a0630f4362866b391ca97f20206dbd05f
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2022-05-20 14:53:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace JsonToXmlParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Xml_RTB = new System.Windows.Forms.RichTextBox();
+            this.Json_RTB = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Xml_RTB
+            // 
+            this.Xml_RTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Xml_RTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Xml_RTB.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Xml_RTB.Location = new System.Drawing.Point(0, 0);
+            this.Xml_RTB.Name = "Xml_RTB";
+            this.Xml_RTB.Size = new System.Drawing.Size(300, 306);
+            this.Xml_RTB.TabIndex = 0;
+            this.Xml_RTB.Text = "";
+            this.Xml_RTB.WordWrap = false;
+            this.Xml_RTB.TextChanged += new System.EventHandler(this.Xml_RTB_TextChanged);
+            // 
+            // Json_RTB
+            // 
+            this.Json_RTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Json_RTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Json_RTB.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Json_RTB.Location = new System.Drawing.Point(0, 0);
+            this.Json_RTB.Name = "Json_RTB";
+            this.Json_RTB.Size = new System.Drawing.Size(302, 306);
+            this.Json_RTB.TabIndex = 0;
+            this.Json_RTB.Text = "";
+            this.Json_RTB.WordWrap = false;
+            this.Json_RTB.TextChanged += new System.EventHandler(this.Json_RTB_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(271, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Json";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(274, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Xml";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.Json_RTB);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.Xml_RTB);
+            this.splitContainer1.Size = new System.Drawing.Size(606, 306);
+            this.splitContainer1.SplitterDistance = 302;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 326);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 21);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Перенос";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(94, 326);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(196, 21);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Отключить конвертирование";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 353);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Xml_RTB;
+        private System.Windows.Forms.RichTextBox Json_RTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+using Newtonsoft.Json;
+
+namespace JsonToXmlParser
+{
+    public partial class Form1 : Form
+    {
+        private bool Converting_ActiveStatys = true;
+        private bool Json_RTB_TextChanged_ActiveStatys = true;
+        private bool Xml_RTB_TextChanged_ActiveStatys = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Json_RTB_TextChanged(object sender, EventArgs e)
+        {
+            if (!Json_RTB_TextChanged_ActiveStatys | !Converting_ActiveStatys) return;
+
+            try
+            {
+                Xml_RTB_TextChanged_ActiveStatys = false;
+                Xml_RTB.Text = GetXml(Json_RTB.Text);
+                Xml_RTB_TextChanged_ActiveStatys = true;
+            }
+            catch (Exception ex) { }
+        }
+
+        private void Xml_RTB_TextChanged(object sender, EventArgs e)
+        {
+            if (!Xml_RTB_TextChanged_ActiveStatys | !Converting_ActiveStatys) return;
+
+            try
+            {
+                Json_RTB_TextChanged_ActiveStatys = false;
+                Json_RTB.Text = GetJson(Xml_RTB.Text);
+                Json_RTB_TextChanged_ActiveStatys = true;
+            }
+            catch (Exception ex) { }
+        }
+
+        private string GetXml(string JsonText)
+        {
+            // To convert JSON text contained in string json into an XML node
+            XmlDocument doc = JsonConvert.DeserializeXmlNode(JsonText);
+            if (doc == null) throw new Exception();
+            return doc.OuterXml;
+        }
+
+        private string GetJson(string XmlText)
+        {
+            // To convert an XML node contained in string xml into a JSON string   
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(XmlText);
+            if (doc == null) throw new Exception();
+            return JsonConvert.SerializeXmlNode(doc);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Json_RTB.WordWrap = checkBox1.Checked;
+            Xml_RTB.WordWrap = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Converting_ActiveStatys = !checkBox2.Checked;
+        }
+    }
+}
